d4d221abaa827f860b4a959e4264fbeb780defbf
Samroon Michael <sam@Samroons-MacBook-Pro.local>
2020-04-06 17:27:05 +0800

    weather-app-react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import SeasonDisplay from './SeasonDisplay/SeasonDisplay';
+import Loader from './Loader/Loader';   
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    state = { lat: null, errorMessage: '' }
+
+    componentDidMount() {
+        window.navigator.geolocation.getCurrentPosition(
+            success => this.setState({ lat: success.coords.latitude }),
+            error => this.setState({ errorMessage: error.message })
+        )
+    }
+
+    renderContent() {
+        if(this.state.errorMessage && !this.state.lat) {
+            return <h1>Error: {this.state.errorMessage}</h1>
+        }
+
+        if(!this.state.errorMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat}/>
+        } 
+
+        return <Loader message="Please allow your location" />
+    }
+
+    render() {
+        return <div>{this.renderContent()}</div>
+    }
 }
 
 export default App;
